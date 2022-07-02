5ea60066db73c7318f875b20be5b79d0d1abfc0b
Vikas Satpute <vikas.s.satpute.com>
2022-07-02 17:02:44 +0530

    feat: added app layout

---
+++ src/components/Layouts/Layout.js
@@ -0,0 +1,16 @@
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import Head from 'next/head';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Layouts </title>
+      </Head>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Dropdown from 'components/Dropdown';
+import Layout from 'components/Layouts/Layout';
+
 export default function Home() {
   return (
     <>
@@ -6,3 +8,7 @@ export default function Home() {
     </>
   );
 }
+
+Home.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
+};
